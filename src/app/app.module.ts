6ac83f5615b6bf17a2a8a7dc5ea86bfc41ed92fd
Nguyen Long <nguyenlong232253@gmail.com>
2022-06-30 15:41:00 +0700

    color-picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MayTinhComponent } from './may-tinh/may-tinh.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
+import {FormsModule} from "@angular/forms";
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import { AngularUsingBootstrapComponent } from './angular-using-bootstrap/angular-using-bootstrap.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MayTinhComponent
+    MayTinhComponent,
+    ColorPickerComponent,
+    FontSizeEditorComponent,
+    AngularUsingBootstrapComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
